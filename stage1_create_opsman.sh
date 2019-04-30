0b4646049b9a52a87c165cc49ddd743ee6b0b37a
Navneet Verma <47264956+papivot@users.noreply.github.com>
2019-04-29 21:22:08 -0400

    Update stage1_create_opsman.sh

--- stage1_create_opsman.sh
+++ stage1_create_opsman.sh
@@ -8,6 +8,22 @@ unzip terraforming*.zip
 cd pivotal-cf-terraforming-*/terraforming-pks
 aws s3 cp s3://$AWS_S3_BUCKET/terraform.tfvars.orig terraform.tfvars
 
+region=`awk '/region/ {print $3}' terraform.tfvars |sed 's/"//g'`
+if [ -z $region ] 
+then 
+    echo "Region missing in Terraform.tfvars file. Exit"
+    exit 1
+fi
+
+original_ami=`awk '/ops_manager_ami/ {print $3}' terraform.tfvars |sed 's/"//g'`
+new_ami=`grep -w $region $DNLDDIR/ops-manager-aws-*.yml|awk '{print $2}'`
+if [ -z $new_ami ] 
+then 
+    echo "AMI entry not found for $region in ops-manager-aws-*.yml file. Exit"
+    exit 1
+fi 
+sed -i "s/${original_ami}/${new_ami}/" terraform.tfvars
+
 terraform init
 terraform plan -out=plan
 terraform apply plan
@@ -19,3 +35,4 @@ fi
 terraform output ops_manager_public_ip
 
 aws s3 cp terraform.tfstate s3://$AWS_S3_BUCKET/terraform.tfstate
+aws s3 cp terraform.tfvars s3://$AWS_S3_BUCKET/terraform.tfvars.orig
