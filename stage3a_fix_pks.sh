f269627c382bdddb89fbda7550aa8f5fbf0dbe00
Navneet Verma <47264956+papivot@users.noreply.github.com>
2019-05-01 12:45:30 -0400

    Update stage3a_fix_pks.sh

--- stage3a_fix_pks.sh
+++ stage3a_fix_pks.sh
@@ -13,19 +13,21 @@ terraform init
 terraform refresh
 OM_IP=`terraform output ops_manager_public_ip`
 
-export vpc_id=`terraform output vpc_id`
-export pks_vm_id=`aws ec2 describe-instances --filters "Name=tag:instance_group,Values=pivotal-container-service"|jq -r '.Reservations[].Instances[].InstanceId'`
-export pks_sg_id=`aws ec2 describe-security-groups --filters "Name=vpc-id,Values=${vpc_id}"|jq -r '.SecurityGroups[]|select (.GroupName == "pks_api_lb_security_group").GroupId'`
-export def_sg_id=`aws ec2 describe-security-groups --filters "Name=vpc-id,Values=${vpc_id}"|jq -r '.SecurityGroups[]|select (.GroupName == "vms_security_group").GroupId'`
+if [ "${CLOUD}" == "AWS" ]
+then
+    export vpc_id=`terraform output vpc_id`
+    export pks_vm_id=`aws ec2 describe-instances --filters "Name=tag:instance_group,Values=pivotal-container-service"|jq -r '.Reservations[].Instances[].InstanceId'`
+    export pks_sg_id=`aws ec2 describe-security-groups --filters "Name=vpc-id,Values=${vpc_id}"|jq -r '.SecurityGroups[]|select (.GroupName == "pks_api_lb_security_group").GroupId'`
+    export def_sg_id=`aws ec2 describe-security-groups --filters "Name=vpc-id,Values=${vpc_id}"|jq -r '.SecurityGroups[]|select (.GroupName == "vms_security_group").GroupId'`
 
-echo "INFO - Fixing Security Group for PKS API Instance ID $pks_vm_id"
-aws ec2 modify-instance-attribute --instance-id $pks_vm_id --groups $pks_sg_id $def_sg_id
+    echo "INFO - Fixing Security Group for PKS API Instance ID $pks_vm_id"
+    aws ec2 modify-instance-attribute --instance-id $pks_vm_id --groups $pks_sg_id $def_sg_id
 
-
-for tgt_grp in $(terraform output pks_api_target_groups)
-do
-    tgt=`echo $tgt_grp|tr -d ,`
-    arn=`aws elbv2 describe-target-groups --name $tgt |jq -r '.TargetGroups[].TargetGroupArn'`
-    echo "INFO - Addoing PKS API Instance ID $pks_vm_id to TG $arn"
-    aws elbv2 register-targets --target-group-arn $arn --targets Id=$pks_vm_id
-done
+    for tgt_grp in $(terraform output pks_api_target_groups)
+    do
+        tgt=`echo $tgt_grp|tr -d ,`
+        arn=`aws elbv2 describe-target-groups --name $tgt |jq -r '.TargetGroups[].TargetGroupArn'`
+        echo "INFO - Addoing PKS API Instance ID $pks_vm_id to TG $arn"
+        aws elbv2 register-targets --target-group-arn $arn --targets Id=$pks_vm_id
+    done
+fi
